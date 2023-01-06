87668712d1f1d3d0da802555c7b2a17c08ea3079
Imtiaz Shawn <Imtiazshawn01@gmail.com>
2023-01-06 11:38:30 +0600

    added another way

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 // 0036. Write a function that takes in an array of numbers and returns the sum of all the elements.
-const numbers = [45, 4, 9, 16, 25];
+    // WAY - 01
+const numbers = [12, 52, 11, 9, 5, 35, 55];
 
 const myFunction = (total, value) => {
   return total + value;
-}
+};
 
-const newNumbers = numbers.reduce(myFunction);
-console.log(newNumbers);
+const totalNumber = numbers.reduce(myFunction);
+console.log(totalNumber);
+
+    // WAY - 02
+const getPlusNumber = (numArray) => {
+  let totalNumber = 0;
+
+  for (let i = 0; i < numArray.length; i++) {
+    totalNumber += numArray[i];
+  }
+  return totalNumber;
+};
+console.log(getPlusNumber([12, 52, 11, 9, 5, 35, 55]));
+
+    // WAY - 03
+const numberss = [12, 52, 11, 9, 5, 35, 55];
+let total = 0;
+
+numberss.forEach(function (value) {
+  total += value;
+});
+
+console.log(total);
 
 
 
@@ -15,55 +37,58 @@ console.log(newNumbers);
 const getLongestString = (stringArray) => {
   let longestString = "";
 
-  for(let i = 0; i < stringArray.length; i++){
-    if(stringArray[i].length > longestString){
+  for (let i = 0; i < stringArray.length; i++) {
+    if (stringArray[i].length > longestString.length) {
       longestString = stringArray[i];
     }
   }
   return longestString;
-}
-
-
-console.log(getLongestString(["Bangladesh", "India", "Pakistan", "Nepal", "Bhutan"]));
+};
+console.log(
+  getLongestString([
+    "Bangladesh",
+    "United States of America",
+    "United Kingdom",
+    "Pakistan",
+    "Poland",
+  ])
+);
 
 
 
 
 // 0038. Find the largest number in an array: Write a function that takes an array of numbers and returns the largest number.
-function getLargestNumber (stringNumbers) {
-  let largestNumber = stringNumbers[0];
+const getLongestNumber = (numbersArray) => {
+  let longestNumber = 0;
 
-  for (let i = 1; i < stringNumbers.length; i++){
-    if(stringNumbers[i] > largestNumber){
-      largestNumber = stringNumbers[i];
+  for (let i = 0; i < numbersArray.length; i++) {
+    if (numbersArray[i] > longestNumber) {
+      longestNumber = numbersArray[i];
     }
   }
-  return largestNumber;
-
-}
-
-console.log(getLargestNumber([45, 22, 13, 55, 60]));
+  return longestNumber;
+};
+console.log(getLongestNumber([1, 5, 6, 8, 2, 3]));
 
 
 
 
 // 0039. Write a function that takes in an array of numbers and returns a new array with all the elements in the original array squared.
-function squareElements(numbers) {
-  let squaredNumbers = [];
+const getSquaredNumber = (normalArray) => {
+  let squaredArray = [];
 
-  for (let i = 0; i < numbers.length; i++) {
-    squaredNumbers.push(numbers[i] ** 2);
+  for (let i = 0; i < normalArray.length; i++) {
+    squaredArray.push(normalArray[i] ** 2);
   }
-  return squaredNumbers;
-}
 
-console.log(squareElements([1, 2, 3, 4, 5]))
-
-
+  return squaredArray;
+};
+console.log(getSquaredNumber([1, 5, 6, 8, 2, 3]));
 
 
 
-// 40. Remove duplicates from an array: Write a function that takes an array and returns a new array with all of the duplicate elements removed.
+
+// 0040. Remove duplicates from an array: Write a function that takes an array and returns a new array with all of the duplicate elements removed.
 function removeDuplicates(arr) {
   let unique = [];
 
